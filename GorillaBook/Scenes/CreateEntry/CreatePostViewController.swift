990b62134bf8bf9a4f27f9cd358e49f57cdd9005
alcamla <camacholaverde@gmail.com>
2020-11-17 08:10:40 -0500

    Allows to add image to the new post

--- GorillaBook/Scenes/CreateEntry/CreatePostViewController.swift
+++ GorillaBook/Scenes/CreateEntry/CreatePostViewController.swift
@@ -7,9 +7,10 @@
 
 import UIKit
 
-final class CreatePostViewController: UIViewController {
+final class CreatePostViewController: UIViewController, UINavigationControllerDelegate {
     @IBOutlet weak var entryTextView: UITextView!
     @IBOutlet weak var characterCountLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     
     var viewModel: CreatePostViewModel!
     
@@ -19,11 +20,15 @@ final class CreatePostViewController: UIViewController {
         entryTextView.delegate = self
         viewModel =  CreatePostViewModel { [self] in
             characterCountLabel.text = viewModel.state.characterCountText
+            imageView.image = viewModel.state.image
         }
         entryTextView.becomeFirstResponder()
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Share", style: .plain, target: self, action: #selector(shareTapped))
     }
-        
+    @IBAction func addPhotoTapped(_ sender: Any) {
+        showPhotos()
+    }
+    
     @objc func  shareTapped() {
         self.delegate?.didCreate(viewModel.state.newFeedEntry)
         self.navigationController?.popViewController(animated:true)
@@ -42,11 +47,33 @@ extension CreatePostViewController: UITextViewDelegate {
     }
 }
 
+extension  CreatePostViewController: UIImagePickerControllerDelegate {
+    func showPhotos(){
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let imagePickerController = UIImagePickerController()
+            imagePickerController.delegate = self;
+            imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        viewModel.imageAdded(info: info)
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+
 struct CreateEntryState {
     var characterCountText: String = "0/150"
     var limitReached = false
     let characterLimit = 150
     var newFeedEntry: FeedEntry?
+    var image: UIImage?
 }
 
 final class CreatePostViewModel {
@@ -75,14 +102,41 @@ final class CreatePostViewModel {
     }
     
     private func updateFeedEntry(with text: String) {
-        var feedEntry = state.newFeedEntry ?? FeedEntry(id: UUID().hashValue, firstName: "Alejandro", lastName: "Camacho", body: text, timestamp: String(Date().timeIntervalSince1970), image: nil)
+        var feedEntry = getOrCreateFeedEntry()
         feedEntry.body = text
         state.newFeedEntry = feedEntry
     }
     
+    private func getOrCreateFeedEntry() -> FeedEntry {
+        return state.newFeedEntry ?? FeedEntry(
+            id: UUID().hashValue,
+            firstName: "Alejandro",
+            lastName: "Camacho",
+            body: "",
+            timestamp: String(Date().timeIntervalSince1970),
+            image: nil
+        )
+    }
+    
     func entryTextChanged(string:String) {
         entryText = string
-        
+    }
+    
+    func imageAdded(info:  [UIImagePickerController.InfoKey : Any]) {
+        if let imgUrl = info[UIImagePickerController.InfoKey.imageURL] as? URL{
+            let imgName = imgUrl.lastPathComponent
+            let documentDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first
+            let localPath = documentDirectory?.appending(imgName)
+            
+            let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+            let data = image.pngData()! as NSData
+            data.write(toFile: localPath!, atomically: true)
+            let photoURL = URL.init(fileURLWithPath: localPath!)
+            var feedEntry = getOrCreateFeedEntry()
+            feedEntry.image = photoURL.absoluteString
+            state.newFeedEntry = feedEntry
+            state.image = image
+        }
     }
     
     func characterCount(for string: String) -> Int {
